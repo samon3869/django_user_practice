6a4a50a0d1a028552a31239f312d98e36093f8bb
KeonHee Lee <power3869@gmail.com>
2021-10-22 23:15:51 +0900

    append 'nickname'field to the User Table and modifiy Signup Form to have nickname iput

--- coplate_project/coplate/admin.py
+++ coplate_project/coplate/admin.py
@@ -4,3 +4,5 @@ from .models import User
 # Register your models here.
 
 admin.site.register(User, UserAdmin)
+# custom fields 라는 섹션 아래에 nicknmae field를 추가!
+UserAdmin.fieldsets += (("Custom fields", {"fields": ("nickname",)}),)
