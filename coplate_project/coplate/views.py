cb0995b33684970d2ef4e6563916723ab7456be2
KeonHee Lee <power3869@gmail.com>
2021-10-23 22:02:14 +0900

    [Change Password Interface]

--- coplate_project/coplate/views.py
+++ coplate_project/coplate/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.urls import reverse
+from allauth.account.views import PasswordChangeView
 
 # Create your views here.
 def index(request):
     return render(request, 'coplate/index.html')
+
+class CustomPasswordChangeView(PasswordChangeView):
+    def get_success_url(self):
+        return reverse('index')
--- coplate_project/coplate_project/urls.py
+++ coplate_project/coplate_project/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from coplate.views import CustomPasswordChangeView
 
 urlpatterns = [
     # admin
@@ -27,6 +28,11 @@ urlpatterns = [
         'email-confirmation-done/',
         TemplateView.as_view(template_name='coplate/email_confirmation_done.html'),
         name="account_email_confirmation_done",
+    ),
+    path(
+        'password/change/', 
+        CustomPasswordChangeView.as_view(), 
+        name="account_password_change",
     ),
     path('', include('allauth.urls')),
 ]
