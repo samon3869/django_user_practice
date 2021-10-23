3b8911139e31c9c9ebd61a7531523b24dce2e40d
KeonHee Lee <power3869@gmail.com>
2021-10-23 21:08:15 +0900

    [email verification] make email verification

--- coplate_project/coplate_project/urls.py
+++ coplate_project/coplate_project/urls.py
@@ -15,9 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    # admin
     path('admin/', admin.site.urls),
+    # coplate
     path('', include('coplate.urls')),
+    # allauth
+    path(
+        'email-confirmation-done/',
+        TemplateView.as_view(template_name='coplate/email_confirmation_done.html'),
+        name="account_email_confirmation_done",
+    ),
     path('', include('allauth.urls')),
 ]
