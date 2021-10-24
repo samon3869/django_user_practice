1474681b1080c48b76ceb66d6c23d3fb199f5450
KeonHee Lee <power3869@gmail.com>
2021-10-24 14:50:36 +0900

    [Review] mediafile settings

--- coplate_project/coplate_project/urls.py
+++ coplate_project/coplate_project/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 from coplate.views import CustomPasswordChangeView
 
 urlpatterns = [
@@ -36,3 +38,5 @@ urlpatterns = [
     ),
     path('', include('allauth.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
