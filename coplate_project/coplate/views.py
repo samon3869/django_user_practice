d92e13c03fc4cb8ec7003646fbf3bbadb6811a3f
KeonHee Lee <power3869@gmail.com>
2021-10-24 22:20:17 +0900

    [Review] use indexview by GenericView

--- coplate_project/coplate/views.py
+++ coplate_project/coplate/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.urls import reverse
+from django.views.generic import ListView
 from allauth.account.views import PasswordChangeView
+from coplate.models import Review
 
 # Create your views here.
-def index(request):
-    return render(request, 'coplate/index.html')
+class IndexView(ListView):
+    model = Review
+    template_name = "coplate/index.html"
+    context_object_name = "reviews"
+    paginate_by = 4
+    ordering = ["-dt_created"]
 
 class CustomPasswordChangeView(PasswordChangeView):
     def get_success_url(self):
