a8fb9736e52f069247ca1348f395eb7f8e1d2eb7
KeonHee Lee <power3869@gmail.com>
2021-10-25 10:54:23 +0900

    [Review] make ReviewDetailView

--- coplate_project/coplate/urls.py
+++ coplate_project/coplate/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name="index"),
+    path(
+    'reviews/<int:review_id>/', 
+    views.ReviewDetailView.as_view(), 
+    name="review-detail",
+    ),
 ]
--- coplate_project/coplate/views.py
+++ coplate_project/coplate/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from allauth.account.views import PasswordChangeView
 from coplate.models import Review
 
@@ -8,9 +8,16 @@ from coplate.models import Review
 class IndexView(ListView):
     model = Review
     template_name = "coplate/index.html"
+    # context_object_name = "review_list" <- 기본값 모델이름_list
     context_object_name = "reviews"
     paginate_by = 4
     ordering = ["-dt_created"]
+
+class ReviewDetailView(DetailView):
+    model = Review
+    template_name = "coplate/review_detail.html"
+    pk_url_kwarg = "review_id"
+    # context_object_name = "review"
 
 class CustomPasswordChangeView(PasswordChangeView):
     def get_success_url(self):
