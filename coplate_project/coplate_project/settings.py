0f9347d7cc7d5765a81c13dd889d4898cf4aee99
KeonHee Lee <power3869@gmail.com>
2021-10-27 15:57:50 +0900

    [Real Email]

--- coplate_project/coplate_project/settings.py
+++ coplate_project/coplate_project/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 import os
+
+# 현재 티렉토리에 위치한 .env 파일로부터 환경변수를 읽어냄
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -155,4 +159,9 @@ ACCOUNT_EMAIL_SUBJECT_PREFIX = ""
 
 # Email settings
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "bbang2bz@gmail.com"
+EMAIL_HOST_PASSWORD = os.environ["BZ_EMAIL_PASSWORD"]
